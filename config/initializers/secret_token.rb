1021a0f4d1976ef6e0a31101c10a3c1da2cedf78
jangelserver <jeangel16@ucatolica.edu.co>
2013-11-22 02:56:30 -0500

    Primer Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Taller2::Application.config.secret_key_base = 'd8d688b0d6eae24f96ae4be2efbd9f81dd15a6d9a24dda79fbf9b97e83dc7e0c6d34299783cd02a67b2a92ac4ae162899a9f3704983585485e414ca3ba641d3a'
